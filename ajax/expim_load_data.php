e09b3c8c8bda9cf8b7daeb47bc830b8831cbac9f
Sednos <gosset-nicolas@orange.fr>
2016-10-20 12:07:33 +0200

    3eme commit, echo Hello world !

--- ajax/expim_load_data.php
+++ ajax/expim_load_data.php
@@ -7,27 +7,60 @@
 
 		global $wpdb;
 
-		$post_type = $_POST["post_type"];
+		$posts_type = $_POST["post_type"];
 
 		$db_posts = $wpdb->posts;
 
-		// echo $db_posts;
-		// exit;
+		//$query_posts = "SELECT * FROM $db_posts WHERE post_type=$posts_type";
 
-		$query_posts = "select * from $db_posts";
+		$result_posts = $wpdb->get_results( 'SELECT * FROM ' . $db_posts . ' WHERE post_type="'.$posts_type.'"');
 
-		$result_posts = $wpdb->get_results( $query_posts );		
+		$nb_checkbox = $wpdb->get_results( 'SELECT * FROM ' . $db_posts );
 
-		var_dump($result_posts);
-?>
-		<table>
-			<tr>
-<?php
+		echo '<table>';
+		echo '<thead>';
+		echo '<tr>';
 
+		foreach ($nb_checkbox as $column) 
+		{
+			echo '<th><input type="checkbox" checked></th>';	
+		}
+
+		echo '</tr>';
+		echo '</thead>';
+		echo '<tbody>';
 		
 		foreach($result_posts as $post)
 		{
+			echo '<tr>';
 			echo '<td>' . $post->ID . '</td>';
+			echo '<td>' . $post->post_author . '</td>';
+			echo '<td>' . $post->post_date . '</td>';
+			echo '<td>' . $post->post_date_gmt . '</td>';
+			echo '<td>' . $post->post_content . '</td>';
+			echo '<td>' . $post->post_title . '</td>';
+			echo '<td>' . $post->post_excerpt . '</td>';
+			echo '<td>' . $post->post_status . '</td>';
+			echo '<td>' . $post->comment_status . '</td>';
+			echo '<td>' . $post->ping_status . '</td>';
+			echo '<td>' . $post->post_password . '</td>';
+			echo '<td>' . $post->post_name . '</td>';
+			echo '<td>' . $post->to_ping . '</td>';
+			echo '<td>' . $post->pinged . '</td>';
+			echo '<td>' . $post->post_modified . '</td>';
+			echo '<td>' . $post->post_modified_gmt . '</td>';
+			echo '<td>' . $post->post_content_filtered . '</td>';
+			echo '<td>' . $post->post_parent . '</td>';
+			echo '<td>' . $post->guid . '</td>';
+			echo '<td>' . $post->menu_order . '</td>';
+			echo '<td>' . $post->post_type . '</td>';
+			echo '<td>' . $post->post_mime_type . '</td>';
+			echo '<td>' . $post->comment_count . '</td>';
+			echo '</tr>';
+
 		}
+
+		echo '</tbody>';
+		echo '</table>';
 		
 	}
